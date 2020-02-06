26b7e4b36c78d96eb79f6cf599618204c316c486
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-02-06 14:50:51 -0300

    Add Cors
    Para o backend ser visivel

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,22 @@ require('dotenv').config({
 
 const express = require('express');
 
+
 class AppController {
     constructor(){
         this.express = express();
-
+        this.cors = require('cors'); //Para o frontend conseguir acessar o backend
+        
         this.middlewares();
         this.routes();
+
         require('./database'); //Não sei onde coloca-lo
+        
+
     }
 
     middlewares(){
+        this.express.use(this.cors({origin: 'http://localhost:3000'}))
         this.express.use(express.json());
     }
 
