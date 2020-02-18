e83c2b3f120d23ce259b3b9338985559feb50d61
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-02-18 13:47:43 -0300

    Atuliazilando a api para funcionar no Emulador do Android

--- mobile/src/service/api.js
+++ mobile/src/service/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: /*'http://192.168.1.20:3333',*/ 'http://192.168.11.7:3333', 
+    // baseURL: 'http://192.168.11.7:3333',  //Matheus
+    // baseURL: 'http://192.168.1.20:3333',  //Jecé
+    baseURL: 'http://10.0.2.2:3333', // Emulador
 });
 
 
